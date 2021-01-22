6ac687dc5da6d2a83c8272c9b3fc31972de3143b
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-01-21 22:01:18 -0600

    Local Storage for Movie Selection and Tickets Price

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
 //Update total and count
  function updateSelectedCount () {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatsCount = selectedSeats.length;
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
@@ -31,5 +43,7 @@ container.addEventListener('click', e => {
 // Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
